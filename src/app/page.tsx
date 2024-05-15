cc5ca0b868b0bca047a1f6d79cbea433a5e9aec3
Danny Lowater 🫶 <git@dannythomas.dev>
2024-05-15 23:10:17 +1000

    to trigger a deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Projects } from "@/components/Projects";
 export default function Home() {
   return (
     <div className="z-10 h-full relative flex flex-col">
+
       <Header />
       <MidSection />
       <div className="flex flex-col gap-5">
