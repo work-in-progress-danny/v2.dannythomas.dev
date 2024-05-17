538f82c633c6b6b15f22248dd737b572d32616e4
Danny Lowater 🫶 <git@dannythomas.dev>
2024-05-18 08:14:18 +1000

    add more colours

--- src/components/pages/home/streak.tsx
+++ src/components/pages/home/streak.tsx
@@ -23,9 +23,11 @@ export const Streak = () => {
 			style={{ height: `${documentHeight}px` }}
 			className="h-max w-1/12 max-w-36 flex flex-row absolute top-0 right-10 z-0 overflow-hidden"
 		>
-			<div className="bg-red-500 flex-grow" />
+			<div className="bg-yellow-400 flex-grow" />
 			<div className="bg-orange-500 flex-grow" />
-			<div className="bg-yellow-500 flex-grow" />
+			<div className="bg-red-600 flex-grow" />
+			<div className="bg-rose-700 flex-grow" />
+			<div className="bg-pink-800 flex-grow" />
 		</div>
 	);
 };
